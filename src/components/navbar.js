1089e21b702b5d4acb46a929858e8c00af11b9eb
Hedy Li <50042066+hedyhyry@users.noreply.github.com>
2020-04-15 13:06:33 +0800

    add data.site.siteMetadata.title to navbar.js

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,54 +1,67 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
 import { css } from "@emotion/core"
 const ListLink = props => (
   <li
     css={css`
       display: inline-block;
       margin-right: 1rem;
-  `}
+    `}
   >
     <Link to={props.to}>{props.children}</Link>
   </li>
 )
 
-export default ({ children }) => (
-  <div
-    css={css`
-      background-color: lavenderblush;
-    `}
-  >
-    <header
+export default ({ children }) => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+  )
+  return (
+    <div
       css={css`
-        margin-bottom: 1.5rem;
+        background-color: lavenderblush;
       `}
     >
-      <Link
-        to="/"
+      <header
         css={css`
-          text-shadow: none;
-          background-image: none;
+          margin-bottom: 1.5rem;
         `}
       >
-        <h3
+        <Link
+          to="/"
           css={css`
-            display: inline;
+            text-shadow: none;
+            background-image: none;
           `}
         >
-          Just a Gatsby Site.
-        </h3>
-      </Link>
-      <ul
-        css={css`
-          list-style: none;
-          float: right;
-        `}
-      >
-        <ListLink to="/">Home</ListLink>
-        <ListLink to="/about/">About</ListLink>
-        <ListLink to="/contact/">Contact</ListLink>
-      </ul>
-    </header>
-    {children}
-  </div>
-)
+          <h3
+            css={css`
+              display: inline;
+            `}
+          >
+            {data.site.siteMetadata.title}
+          </h3>
+        </Link>
+        <ul
+          css={css`
+            list-style: none;
+            float: right;
+          `}
+        >
+          <ListLink to="/">Home</ListLink>
+          <ListLink to="/about/">About</ListLink>
+          <ListLink to="/contact/">Contact</ListLink>
+        </ul>
+      </header>
+      {children}
+    </div>
+  )
+}
