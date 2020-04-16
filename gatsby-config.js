345145ecdf10822decf835204e7e04559695569f
Hedy Li <50042066+hedyhyry@users.noreply.github.com>
2020-04-16 17:02:31 +0800

    add react-helmet and source-filesystem plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,14 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-emotion`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
     {
       resolve: `gatsby-plugin-typography`,
       options: {
