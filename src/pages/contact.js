e04f9b8b5929336038276f70a7fea6438ee053f2
Hedy Li <50042066+hedyhyry@users.noreply.github.com>
2020-04-17 12:23:19 +0800

    modify header text in contact to Contact Page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,7 +6,7 @@ import Layout from "../components/layout"
 export default () => (
   <Layout>
     <div>
-      <Header>Contact Me</Header>
+      <Header>Contact Page</Header>
       <p>Hello, this is my contact page! </p>
     </div>
   </Layout>
