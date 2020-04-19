ce5e4e2a728c822aa8038b377d4449f822a114d1
Hedy <hedyhyry@gmail.com>
2020-04-20 07:43:40 +0800

    remove iss#1 fixme comment as the issue is closed
    it was solved with changing the @emotion/core css into jsx css, don't know why it didn't work before, but now it works.

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,7 @@ import Navbar from "../components/navbar"
 import { css } from "@emotion/core"
 
 export default ({ children }) => (
-  <div // FIXME #1 margin and max width etc css not working (index.js)
+  <div
     style={{ margin: `3rem auto`, maxWidth: 880 }}
     css={css`
       margin: 3rem auto;
